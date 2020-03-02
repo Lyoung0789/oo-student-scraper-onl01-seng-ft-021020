dcf50febaf5567b616a63acb25e25d6e3196589e
Lester Young <lyoung070789@gmail.com>
2020-03-02 04:59:40 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -31,9 +31,10 @@ class Scraper
     social = doc.css("div.vitals-container .social-icon-container a")
     social.each do |index|
       binding.pry
+    
       links[:twitter] = index.attribute("href").value
       links[:linkedin] = index.attribute("href").value
-      links[:github] = 
+      # links[:github] = 
       links[]
       binding.pry
   
