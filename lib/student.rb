4cc8080050c6b87f3122e609c308ea07669aefc1
Lester Young <lyoung070789@gmail.com>
2020-03-02 05:49:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,15 +11,12 @@ class Student
       # binding.pry
     end 
     @@all << self
-    
   end
 
   def self.create_from_collection(students_array)
     students_array.each do |student|
       self.new(student)
     end 
-    
-  
   end
 
   def add_student_attributes(attributes_hash)
