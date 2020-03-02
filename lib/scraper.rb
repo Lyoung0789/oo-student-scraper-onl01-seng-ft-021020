383077aedea95af164b74c014e196aa39fc9df77
Lester Young <lyoung070789@gmail.com>
2020-03-02 03:28:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,9 @@ require 'pry'
 require 'nokogiri'
 
 class Scraper
-
+  @@all = []
+  attr_accessor :name, :location, :profile
+  
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
