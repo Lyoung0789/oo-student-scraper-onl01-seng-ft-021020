1a57e9af69cce58b72ab0ed585a559fef9e54955
Lester Young <lyoung070789@gmail.com>
2020-03-02 05:21:36 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -42,7 +42,7 @@ class Scraper
        end 
     end 
     
-    links[:profile_quote] = doc.css("div.vitals-container .vitals-text-container div.profile-quote")
+    links[:profile_quote] = doc.css("div.vitals-container .vitals-text-container div.profile-quote").text
     
     
     binding.pry
